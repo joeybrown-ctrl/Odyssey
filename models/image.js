ddbc7090e775386c571820c289c0e8d85185f393
alhoff <andrew.hoff14@yahoo.com>
2020-11-19 19:39:07 -0700

    Image url saved into db

--- models/image.js
+++ models/image.js
@@ -1,7 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
   const Image = sequelize.define("Image", {
     image: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
     },
 
