fa87cfc8902ce39482893e89f8d7239e4251af40
alhoff <andrew.hoff14@yahoo.com>
2020-11-19 22:35:30 -0700

    Images are pulled from db and display on page, albiet incorrectly

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 
-// const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary");
 
-// cloudinary.config({
-//   // eslint-disable-next-line camelcase
-//   cloud_name: "odysseyuserimages",
-//   // eslint-disable-next-line camelcase
-//   api_key: process.env.cloudinaryApiKey,
-//   // eslint-disable-next-line camelcase
-//   api_secret: process.env.cloudinarySecretKey,
-// });
+cloudinary.config({
+  // eslint-disable-next-line camelcase
+  cloud_name: "odysseyuserimages",
+  // eslint-disable-next-line camelcase
+  api_key: process.env.cloudinaryApiKey,
+  // eslint-disable-next-line camelcase
+  api_secret: process.env.cloudinarySecretKey,
+});
 
 // Requiring necessary npm packages
 const express = require("express");
@@ -21,7 +21,7 @@ const passport = require("./config/passport");
 const routes = require("./controllers");
 
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 // Bringing in Morgan, a nice logger for our server
@@ -54,7 +54,7 @@ app.use(routes);
 
 const config = { force: true };
 if (process.env.NODE_ENV === "test") {
-  config.force = true;
+  config.force = false;
 }
 // if we need it! {force:true}
 // Syncing our database and logging a message to the user upon success
